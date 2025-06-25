9dd847501cb7ba57bde83bcf4a2ea0ea4450b37c
aminelhm <131161302+aminelhm@users.noreply.github.com>
2025-06-25 15:16:50 +0200

    passage au nouveau format eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,21 @@
-// eslint.config.js
-import js from '@eslint/js';
-import ts from 'typescript-eslint';
+import js from '@eslint/js'
+import ts from 'typescript-eslint'
 
 export default [
   js.configs.recommended,
   ...ts.configs.recommended,
   {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'coverage/',
+      'prisma/generated/',
+      '*.config.js',
+      '*.config.cjs',
+      'jest.config.js',
+    ],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
-];
+]
